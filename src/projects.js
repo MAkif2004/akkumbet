ccd9b362bfadb006e42101a2eeca4a6f7003398b
gogema <akif.goge@hva.nl>
2025-01-26 12:28:16 +0100

    fullscreen images

--- src/projects.js
+++ src/projects.js
@@ -4,6 +4,12 @@ function onload() {
     const rightArrow = document.getElementsByClassName('right-arrow');
     const leftArrow = document.getElementsByClassName('left-arrow');
     const imgContainers = document.getElementsByClassName('img-container');
+
+    const imgs = document.getElementsByClassName('image');
+    const fullscreenContainer = document.getElementById('fullscreen-container');
+    const fullscreenImgContainer = document.getElementById('fullscreen-image-container');
+    const imageCloseButton = document.getElementById('image-close-button');
+
     for (let i = 0; i < rightArrow.length; i++) {
         rightArrow[i].addEventListener('click', () => {
             changeImg(imgContainers[i], true);
@@ -14,6 +20,29 @@ function onload() {
             changeImg(imgContainers[i], false);
         })
     }
+
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].addEventListener('click', () => {
+            toggleFullscreenContainer(imgs[i], fullscreenContainer, fullscreenImgContainer);
+        })
+    }
+    imageCloseButton.addEventListener('click', () => {
+        toggleFullscreenContainer(null, fullscreenContainer, fullscreenImgContainer);
+    })
+    fullscreenContainer.addEventListener('click', () => {
+        toggleFullscreenContainer(null, fullscreenContainer, fullscreenImgContainer);
+    })
+}
+
+function toggleFullscreenContainer(img = null, fullscreenContainer, fullscreenImgContainer) {
+    if (fullscreenContainer.classList.contains('hidden')) {
+        document.body.style.overflowY = 'hidden';
+        fullscreenImgContainer.style.backgroundImage = `url(${img.src})`;
+    }
+    else {
+        document.body.style.overflowY = 'auto';
+    }
+    fullscreenContainer.classList.toggle("hidden");
 }
 
 function changeImg(imgContainer, next) {
